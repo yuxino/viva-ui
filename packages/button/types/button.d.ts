9f08c4ec91a97d7bee1629244eb7f1ea9292aadd
yuxino <nbsawz@gmail.com>
2019-08-26 18:23:57 +0800

    ignore types

--- packages/button/types/button.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: ({ name }: {
-    name: string;
-}) => any;
-/** this is a cute button componet XD */
-export default _default;
--- packages/ui/types/packages/ui/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Alert from "@viva-ui/alert";
-import Button from "@viva-ui/button";
-export { Alert, Button };
