cb49418f72f4bac7b2eec80aee66159b44189ed5
yuxino <nbsawz@gmail.com>
2019-09-06 18:50:40 +0800

    chore: modify dialog style

--- packages/dialog/src/dialog.tsx
+++ packages/dialog/src/dialog.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 import { useState } from "react";
 import { Emitter } from "@viva-ui/shared";
 
+interface DialogProps {
+  title: String;
+}
+
 const emitter = new Emitter();
 
-const Dialog = () => {
-  const [visiable, setVisiable] = useState(false);
-  const [comp, setComp] = useState(null);
+const useDialog = (v = false) => {
+  const [visiable, setVisiable] = useState(v);
+  const [component, setComp] = useState(null);
   emitter.on("openDialog", component => {
     const Component =
       typeof component === "function" ? component : () => component;
@@ -14,11 +18,27 @@ const Dialog = () => {
     setComp(<Component />);
   });
   emitter.on("closeDialog", () => setVisiable(false));
-  return visiable ? <div className="div">{comp}</div> : null;
+  return [visiable, component];
 };
 
 const openDialog = component => emitter.emit("openDialog", component);
 const closeDialog = () => emitter.emit("closeDialog");
+
+const Dialog = ({ title }: DialogProps) => {
+  const [visiable, component] = useDialog();
+
+  return (
+    visiable && (
+      <div className="viva-ui-dialog">
+        <div className="viva-ui-dialog_box">
+          <div className="viva-ui-dialog_close"></div>
+          <div className="viva-ui-dialog_title">{title}</div>
+          <div>{component}</div>
+        </div>
+      </div>
+    )
+  );
+};
 
 export { openDialog, closeDialog };
 export default Dialog;
--- stories/Dialog.tsx
+++ stories/Dialog.tsx
@@ -2,21 +2,32 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Dialog, openDialog, closeDialog } from "../packages/ui";
 
-storiesOf("Dialog", module).add("Basic", () => (
-  <div>
-    <Dialog />
-    <button
-      onClick={() =>
-        openDialog(
-          <div key="fuck" style={{ marginBottom: 15 }}>
-            I am the mother fuck dialog
-          </div>
-        )
-      }
-      style={{ marginRight: 15 }}
-    >
-      click me to show dialog
-    </button>
-    <button onClick={() => closeDialog()}>click me to hide dialog</button>
-  </div>
-));
+storiesOf("Dialog", module).add("Basic", () => {
+  const style = {
+    body: {
+      padding: 25,
+      textAlign: "center"
+    },
+    p: {
+      margin: 0,
+      marginBottom: 15
+    }
+  };
+  const body = (
+    <div key="keyY" style={style.body}>
+      <p style={style.p}>A personal markdown editor by Yuxino</p>
+      <p style={style.p}>You can find me in follow way</p>
+      <p style={style.p}>Github, Slack, Telgram etc ...</p>
+      <div>version 1.0.0</div>
+    </div>
+  );
+  return (
+    <div>
+      <Dialog title="viva editor" />
+      <button onClick={() => openDialog(body)} style={{ marginRight: 15 }}>
+        click me to show dialog
+      </button>
+      <button onClick={() => closeDialog()}>click me to hide dialog</button>
+    </div>
+  );
+});
