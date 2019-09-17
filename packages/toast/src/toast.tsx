4aaf5039a06b7eda1ea227c2ca28a7fc3b439afd
yuxino <nbsawz@gmail.com>
2019-09-17 17:48:23 +0800

    feat: add colors in theme & modify toast style

--- packages/toast/src/toast.tsx
+++ packages/toast/src/toast.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 
+interface ToastProps {
+  children?: any;
+  type?: "nomarl" | "success" | "warning" | "error";
+}
+
 /** this is a cute button componet XD */
-export default ({ name }: { name: string }) => {
-  return <button>hello {name} !!</button>;
+export default ({ children, type }: ToastProps) => {
+  return <div className={`viva-ui-toast ${type}`}>{children}</div>;
 };
--- stories/Toast.tsx
+++ stories/Toast.tsx
@@ -2,4 +2,11 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Toast } from "@viva-ui/ui";
 
-storiesOf("Toast", module).add("Basic", () => <Toast name="123">123</Toast>);
+storiesOf("Toast", module).add("Basic", () => (
+  <>
+    <Toast type="nomarl">The Evil Rabbit jumped over the fence</Toast>
+    <Toast type="success">The Evil Rabbit jumped over the fence</Toast>
+    <Toast type="warning">The Evil Rabbit jumped over the fence</Toast>
+    <Toast type="error">The Evil Rabbit jumped over the fence</Toast>
+  </>
+));
